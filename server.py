ce6cdb6a42fb1df46d996e689fd572162c7eb8c8
alexinyew <csimmons11121@gmail.com>
2022-07-21 10:20:15 -0400

    initial commit

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import asyncio
+from pydoc import doc
+from statistics import mode
 import websockets
 import json
 import signal
 import os
+import pprint
+
+userDetails = {}
 
 connected = set()
 
@@ -13,23 +18,42 @@ async def server(websocket, path):
     try:
         async for message in websocket:
             message = json.loads(message)
-            print(message)
+            userId = message["userId"]
+            Map = message["Map"]
+            Mode = message["Mode"]
+
+            userDetails[websocket]["userId"] = userId
+            userDetails[websocket]["Map"] = Map
+
+            if(Mode == "mapChange"):
+                for user in connected:
+                    if user != websocket:
+                        data = {'Mode': 'Disconnect', 'userId': userDetails[websocket]["userId"],
+                         'Map': userDetails[websocket]["Map"]}
+
+            elif(Mode == "Move"):
+                for user in connected:
+                    if user != websocket:
+                        if userDetails[user]["Map"] == userDetails[websocket]["Map"]: #On the same map
+                            Data = message["Data"]
+                            user.send(json.dumps(userId,Mode,Data))
+                
+
+            
+
     finally:
         print("Client disconnected")
+        for user in connected:
+            if user != websocket:
+                data = {'Mode': 'Disconnect', 'userId': userDetails[websocket]["userId"]}
+                user.send(json.dumps(data))
+
         connected.remove(websocket)
+        del userDetails[websocket]
 
 async def main():
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-    async with websockets.serve(
-        server,
-        host="",
-        port=int(os.environ["PORT"]),
-    ):
-        await stop
-
+    async with websockets.serve(server, "localhost", 3000):
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
