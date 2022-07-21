db23518d60e613e7d138d6b4e91782be76894718
alexinyew <csimmons11121@gmail.com>
2022-07-21 11:14:05 -0400

    a

--- server.py
+++ server.py
@@ -34,8 +34,8 @@ async def server(websocket, path):
 
             elif(Mode == "Move"):
                 for user in connected:
-                    if user != websocket:
-                        if userDetails[user]["Map"] == userDetails[websocket]["Map"]: #On the same map
+                    if user == websocket:
+                        if userDetails[user]["Map"] == userDetails[websocket]["Map"]:
                             Data = message["Data"]
                             data = {'userId': userDetails[websocket]["userId"], 'Mode': "Move", 'Data': Data}
                             await user.send(json.dumps(data))
