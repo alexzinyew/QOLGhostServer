f5a9322ee39f3704d7aff2ccaf041ffcdc020bb7
alexinyew <csimmons11121@gmail.com>
2022-07-20 20:30:22 -0400

    heroku port

--- server.py
+++ server.py
@@ -1,3 +1,35 @@
-from flask import Flask
-from flask_sock import Sock
+import asyncio
+import websockets
+import json
+import signal
+import os
 
+connected = set()
+
+async def server(websocket, path):
+
+    connected.add(websocket)
+    print("Client connected!") 
+    try:
+        async for message in websocket:
+            message = json.loads(message)
+            print(message)
+    finally:
+        print("Client disconnected")
+        connected.remove(websocket)
+
+async def main():
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    async with websockets.serve(
+        server,
+        host="localhost",
+        port=int(os.environ["PORT"]),
+    ):
+        await stop
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
